b73a37d0a5e2ae73264fcec33cb6f718d203e424
jt95004w <jtsox04@gmail.com>
2025-01-22 19:24:14 -0500

    Heading and hero work only

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 console.log("js loaded");
+
+//For coloring nav links. Removes 'active' from all nav links and adds to clicked link
+document.querySelectorAll('.main-nav a').forEach(link => {
+    link.addEventListener('click', (e) => {
+      // Remove 'active' from all links
+      document.querySelectorAll('.main-nav a').forEach(el => el.classList.remove('active'));
+      // Add 'active' to the clicked link
+      e.currentTarget.classList.add('active');
+    });
+  });
