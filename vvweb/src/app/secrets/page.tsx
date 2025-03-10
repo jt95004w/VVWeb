0094f523d97612d83e58103d848a7fc070761831
Vrishabhshri <vrishabhshri@gmail.com>
2025-03-09 23:12:30 -0400

    Adding secrets page

---
+++ vvweb/src/app/secrets/page.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { submissions as importedSubmissions } from '../../../public/jsons/Submission.json';
+
+interface Submission {
+
+    id: number;
+    email: string;
+    message: string;
+
+}
+
+export default function Secrets() {
+
+    return (
+
+        <div className="p-6">
+        <h1 className="text-3xl font-bold mb-6">Form Submissions</h1>
+            <div className="space-y-6">
+                {importedSubmissions.map((submission) => (
+                <div
+                    key={submission.id}
+                    className="p-4 border border-gray-300 rounded-lg shadow-md hover:bg-white hover:text-black transition-all duration-300"
+                >
+                    <h2 className="text-xl font-semibold">{submission.email}</h2>
+                    <p className="text-gray-700 mt-2">{submission.message}</p>
+                </div>
+                ))}
+            </div>
+        </div>
+
+    )
+
+}
